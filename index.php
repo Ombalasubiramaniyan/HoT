9229adeb59438ebc9b73bb01dde5d22d0178a309
Ombalasubiramaniyan <140371267+Ombalasubiramaniyan@users.noreply.github.com>
2023-07-24 10:58:56 +0530

    index.php
    index page

---
+++ index.php
@@ -0,0 +1,388 @@
+<?php
+include "config.php";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>High on Trend</title>
+   <!--my links and files -->
+    <link rel="stylesheet" href="fontawesome-free-6.2.1-web/css/all.min.css">
+    <link rel="stylesheet" href="bootstrap-5.3.0-alpha1-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="fontawesome-free-6.2.1-web/css/fontawesome.min.css">
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="icon" href="img/logo.jpg" type=image/x-icon>
+</head>
+
+<body>
+    <!--nav par -->
+    <section id="home">
+            <div id="bg">
+            <nav class="nav">
+                    <h2 class="brand">High On Trend </h2>
+                    <ul class="menu">
+                        <li class="home"><a href="index.php">Home</a></li>
+                        <li class="home"><a href="fashion.php">Fashion</a></li>
+                        <li class="home"><a href="electronics.php">Eletronics</a></li>
+                        <li class="home"><a href="about.php" style="margin-right:300px">About</a></li>
+                        <li class="logo"><a href="viewcart.php"><i class="fa fa-shopping-cart"></i></a></li>
+                        <li class="login"><a href="reg.html"><i class="fa-solid fa-user"></i></a></li>
+                    </ul>
+                </nav>
+                <!--nav end-->
+                <div class="header-main" id="home">
+                    <!-- header main content -->
+                    <div class="header-main-content">
+                        <div class="header-main-list">
+                            <div class="header-main-item" data-id="1">
+                                <h1>Trending For Youths </h1>
+                                <p class="text">Latest collections and accessories20% off using credit card</p>
+                                <a href="acc.php" class="btn">Discover more</a>
+                            </div>
+                            <div class="header-main-item" data-id="2">
+                                <h1>Traditional & Models Cloths</h1>
+
+                                <p class="text">New Trending Collections ,Saree and Traditional
+                                     wears for everyone Are Available start From Rs.199/-</p>
+                                <a href="fashion.php" class="btn">Discover more</a>
+                            </div>
+                            <div class="header-main-item" data-id="3">
+                                <h1>Top Deal On Electronics</h1>
+                                <p class="text">All Electronics And Accessories,Moblies 
+                                    ,Laptops,Taplets,Cameras start from Rs.399/-</p>
+                                <a href="electronics.php" target="_blank" class="btn">Discover more</a>
+                            </div></div> </div>
+                    <!-- end of header main content --></div></div>
+            <button type="button" class="header-left-btn flex"><i class="fas fa-chevron-left"></i>
+      </button>
+            <button type="button" class="header-right-btn flex"><i class="fas fa-chevron-right"></i>
+      </button>
+    </section>
+    <!-- ***** Main Banner Area End ***** -->
+    <div class="main-banner">
+        <h4>HIGH ON TREND BUY ON TREND</h4>
+    </div>
+    <div class="main-banner" id="top">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="left-content">
+                        <div class="thumb">
+                            <div class="inner-content">
+                                <h4>HIGH ON TREND</h4>
+                                <span>Awesome, Unique &amp; High Quality Productes</span>
+                                <div class="main-border-button">
+                                    <a href="#">Purchase Now!</a>
+                                </div>
+                            </div>
+                            <img src="img/left-banner-image.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="right-content">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <div class="right-first-image">
+                                    <div class="thumb">
+                                        <div class="inner-content">
+                                            <h4>Men</h4>
+                                            <span>Best Clothes For Men</span>
+                                        </div>
+                                        <div class="hover-content">
+                                            <div class="inner">
+                                                <h4>Men</h4>
+                                                <p>Trending and traditional outfits for mens</p>
+                                                <div class="main-border-button">
+                                                    <a href="#">Discover More</a></div>
+                                            </div>
+                                        </div>
+                                        <img src="img/baner-right-image-02.jpg">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="right-first-image">
+                                    <div class="thumb">
+                                        <div class="inner-content">
+                                            <h4>Mobiles</h4>
+                                            <span>Best New For Moblies</span>
+                                        </div>
+                                        <div class="hover-content">
+                                            <div class="inner">
+                                                <h4>Mobiles</h4>
+                                                <p>Low price and high quality Mobiles</p>
+                                                <div class="main-border-button">
+                                                    <a href="electronics.php">Discover More</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <img src="img/mob.jpg">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="right-first-image">
+                                    <div class="thumb">
+                                        <div class="inner-content">
+                                            <h4>Kids</h4>
+                                            <span>Best outfits For Kids</span>
+                                        </div>
+                                        <div class="hover-content">
+                                            <div class="inner">
+                                                <h4>Kids</h4>
+                                                <p></p>
+                                                <div class="main-border-button">
+                                                    <a href="fashion.php">Discover More</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <img src="img/54generic.jpg" width=30%; >
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="right-first-image">
+                                    <div class="thumb">
+                                        <div class="inner-content">
+                                            <h4>Accessories</h4>
+                                            <span>Best Trend Accessories</span>
+                                        </div>
+                                        <div class="hover-content">
+                                            <div class="inner">
+                                                <h4>Accessories</h4>
+                                                <p>stylish gajects.</p>
+                                                <div class="main-border-button">
+                                                    <a href="acc.php">Discover More</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <img src="img/acc.jpg">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- ***** Main Banner Area End ***** -->
+     <!-- ***** Explore Area Starts ***** -->
+     <section class="section" id="explore">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="left-content">
+                        <h2>Explore Our Products</h2>
+                        <span>You are allowed to use this E-commerce website and you can purchase gadgets,trendy outfits for women,kids and mens in low price and high quality.  </span>
+                        <br>
+                        <div class="quote">
+                     
+                            <i class="fa fa-quote-left"></i><p>You are not allowed to suspicious works and you cannot get a Refund . </p>
+                        </div>
+                        <br>
+                        <p>There are many collections in electric gadgets and fashion items and accessories also .</p>
+                        <br>
+                        <hr>
+                        <br>
+                        <br>
+
+                        <div class="main-border-button">
+                            <a href="index.php">Discover More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="right-content">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <div class="leather">
+                                    <h4>Leather Bags</h4>
+                                    <span>Latest Collection</span>
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="first-image">
+                                    <img src="img/cam1.jpg" alt="">
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="second-image">
+                                    <img src="img/lap1.jpg" alt="">
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="types">
+                                    <h4>Different Types</h4>
+                                    <span>Over 304 Products</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- ***** Explore Area Ends ***** -->
+
+    <!-- ***** Social Area Starts ***** -->
+    <section class="section" id="social">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="section-heading">
+                        <h2>Social Media</h2>
+                        <span>you can contact our customer service on any social media platforms.</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row images">
+                <div class="col-2">
+                    <div class="thumb">
+                        <div class="icon" >
+                            <a href="http://facebook.com" style="text-decoration: none;color:aqua;">
+                                <h6>Fashion</h6>
+                                <i class="fa-brands fa-facebook-f"></i>
+                               
+                            </a>
+                        </div>
+                        <img src="img/acc.jpg" alt="">
+                    </div>
+                </div>
+                <div class="col-2">
+                    <div class="thumb">
+                        <div class="icon">
+                            <a href="http://facebook.com" style="text-decoration: none;">
+                                <h6>New</h6>
+                                <i class="fa-brands fa-facebook-f"></i>
+                            </a>
+                        </div>
+                        <img src="img/service-01.jpg" alt="">
+                    </div>
+                </div>
+                <div class="col-2">
+                    <div class="thumb">
+                        <div class="icon">
+                            <a href="http://facebook.com" style="text-decoration: none; color:aqua;">
+                                <h6>Brand</h6>
+                                <i class="fa fa-instagram"></i>
+                            </a>
+                        </div>
+                        <img src="img/left-banner-image.jpg" alt="">
+                    </div>
+                </div>
+                <div class="col-2">
+                    <div class="thumb">
+                        <div class="icon">
+                            <a href="http://facebook.com" style="text-decoration: none; color:aqua;">
+                                <h6>Makeup</h6>
+                                <i class="fa-brands fa-facebook-f"></i>
+                            </a>
+                        </div>
+                         <img src="img/service-02.jpg" alt="">
+                       
+                    </div>
+                </div>
+                <div class="col-2">
+                    <div class="thumb">
+                        <div class="icon">
+                            <a href="http://facebook.com" style="text-decoration: none; color:aqua;">
+                                <h6>Leather</h6>
+                                <i class="fa-brands fa-facebook-f"></i>
+                            </a>
+                        </div>
+                        <img src="img/baner-right-image-04.jpg" alt="">
+                    </div>
+                </div>
+                <div class="col-2">
+                    <div class="thumb">
+                        <div class="icon">
+                            <a href="http://facebook.com" style="text-decoration: none; color:aqua;">
+                                <h6>Bag</h6>
+                                <i class="fa-brands fa-facebook-f"></i>
+                            </a>
+                        </div>
+                        <img src="img/service-03.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- ***** Social Area Ends ***** -->
+
+    <!--about area-->
+    <section id="about">
+        <footer>
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-3">
+                        <div class="first-item">
+                            <h2 style="color: #fff">High-On-Trend</h2>
+                            <ul>
+                                <li><a href="#">Arumugam Pillai Sethai Ammal College Sivaganga(DT) TamilNadu India Pvt</a></li>
+                                <li><a href="#">highOntrend.com</a></li>
+                                <li><a href="#">6383118589</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                        <h4>Shopping &amp; Categories</h4>
+                        <ul>
+                            <li><a href="#men">Mens Shopping</a></li>
+                            <li><a href="#women">Womens Shopping</a></li>
+                            <li><a href="#kids">Kids Shopping</a></li>
+                            <li><a href="#Eletronics">Electroincs</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-3">
+                        <h4>Useful Links</h4>
+                        <ul>
+                            <li><a href="#home">Homepage</a></li>
+                            <li><a href="#about">About Us</a></li>
+                            <li><a href="log.html" target="_blank">login</a></li>
+                            <li><a href="#fashion">products Us</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-3">
+                        <h4>Help &amp; Information</h4>
+                        <ul>
+                            <li><a href="https://google.com" target="_blank">Help</a></li>
+                            <li><a href="https://google.com" target="_blank">FAQ's</a></li>
+                            <li><a href="#home">Shipping</a></li>
+                            <li><a href="#about">Tracking ID</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-12">
+                        <div class="under-footer">
+                            <p>Copyright © 2022 high on trend., Ltd. All Rights Reserved.
+
+                                <br>Design: <a href="https://google.com" target="_blank" title="free css templates">HOT</a>
+
+                                <br>Distributed By: <a href="https://amazon.com" target="_blank" title="all products are available">shoppingOrg</a>
+                            </p>
+                            <ul>
+                                <li><a href="https://facebook.com" target="_blank"><i class="fa-brands fa-facebook-f"></i></a>
+                                </li>
+                                <li><a href="https://twitter.com" target="_blank"><i class="fa-brands fa-twitter"></i></a></li>
+                                <li><a href="https://linkedin.com" target="_blank"><i class="fa-brands fa-linkedin-in"></i></a></li>
+                                <li><a href="https://behance.com" target="_blank"><i class="fa-brands fa-behance"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </footer>
+    </section>
+
+
+
+    <script src="index.js"></script>
+</body>
+
+</html>

87732f4dd3fbe3aef686ea20f32f41bc5be637bc
Ombalasubiramaniyan <140371267+Ombalasubiramaniyan@users.noreply.github.com>
2023-07-24 11:34:40 +0530

    header.php

---
+++ header.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <title>High on Trend</title>
+    <link rel="stylesheet" href="fontawesome-free-6.2.1-web/css/all.min.css">
+    <link rel="stylesheet" href="bootstrap-5.3.0-alpha1-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="fontawesome-free-6.2.1-web/css/fontawesome.min.css">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="icon" href="img/logo.jpg" type=image/x-icon>
+    </head>
+    <body>
+        <header>
+        <html>
+
+    <!--nav par -->
+<div class="par">
+        <header>
+            
+                <nav class="nav">
+                    <h2 class="brand">
+                        High On Trend
+                    </h2>
+                    <ul class="menu">
+                        <li class="home">
+                            <a href="index.php">Home</a>
+                        </li>
+                        <li class="home">
+                            <a href="fashion.php">Fashion</a>
+                        </li>
+                        <li class="home">
+                            <a href="electronics.php">Eletronics</a>
+                        </li>
+                        <li class="home">
+                            <a href="about.php" style="margin-right:300px">About</a>
+                        </li>
+
+                        <li class="logo">
+                            <a href="viewcart.php"><i class="fa fa-shopping-cart"></i></a>
+                        </li>
+                        <li class="login">
+                            <a href="reg.html">
+                                <i class="fa-solid fa-user"></i></a>
+                        </li>
+                    </ul>
+                </nav>
+        </header>
+        </div>
+    
+    </body>
+</html>

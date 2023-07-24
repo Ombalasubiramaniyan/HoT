a7b91a54fdff2f855982c291dcdb9f4466280933
Ombalasubiramaniyan <140371267+Ombalasubiramaniyan@users.noreply.github.com>
2023-07-24 11:05:17 +0530

    Create about.php

---
+++ about.php
@@ -0,0 +1,188 @@
+<?php
+include "config.php";
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="fontawesome-free-6.2.1-web/css/all.min.css">
+    <link rel="stylesheet" href="bootstrap-5.3.0-alpha1-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="fontawesome-free-6.2.1-web/css/fontawesome.min.css">
+    <link rel='stylesheet'  href='css/about.css'>
+    <link rel="stylesheet" href="css/header.css">
+    </head>
+    <body>
+    <div class="par">
+        <header>
+            
+                <nav class="nav">
+                    <h2 class="brand">
+                        High On Trend
+                    </h2>
+                    <ul class="menu">
+                        <li class="home">
+                            <a href="index.php">Home</a>
+                        </li>
+                        <li class="home">
+                            <a href="fashion.php">Fashion</a>
+                        </li>
+                        <li class="home">
+                            <a href="electronics.php">Eletronics</a>
+                        </li>
+                        <li class="home">
+                            <a href="about.php" style="margin-right:300px">About</a>
+                        </li>
+
+                        <li class="logo">
+                            <a href="viewcart.php"><i class="fa fa-shopping-cart"></i></a>
+                        </li>
+                        <li class="login">
+                            <a href="reg.html">
+                                <i class="fa-solid fa-user"></i></a>
+                        </li>
+                    </ul>
+                </nav>
+        </header>
+        </div>
+ 
+            <div class="page-heading about-page-heading" id="top">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="inner-content">
+                        <h2>About Our Company</h2>
+                        <span>High-On-Trend</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- ***** Main Banner Area End ***** -->
+
+    <!-- ***** About Area Starts ***** -->
+    <div class="about-us">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="left-image">
+                        <img src="img/service-01.jpg" alt="">
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="right-content">
+                        <h4>About Us &amp; Our Skills</h4>
+                        <span>You are allowed to use this E-commerce website and you can purchase gadgets,trendy outfits for women,kids and mens in low price and high quality.</span>
+                        <div class="quote">
+                            <i class="fa fa-quote-left"></i><p>You are not allowed to suspicious works and you cannot get a Refund .</p>
+                        </div>
+                        <p>There are many collections in electric gadgets and fashion items and accessories also .E-commerce website and you can purchase gadgets,trendy outfits for women,kids and mens in low price</p>
+                        <ul>
+                        
+                                <li><a href="https://facebook.com" target="_blank"><i class="fa-brands fa-facebook-f"></i></a>
+                                </li>
+                                <li><a href="https://twitter.com" target="_blank"><i class="fa-brands fa-twitter"></i></a></li>
+                                <li><a href="https://linkedin.com" target="_blank"><i class="fa-brands fa-linkedin-in"></i></a></li>
+                                <li><a href="https://behance.com" target="_blank"><i class="fa-brands fa-behance"></i></a></li>
+                          
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- ***** About Area Ends ***** -->
+    
+    <!-- ***** Services Area Starts ***** -->
+    <section class="our-services">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="section-heading">
+                        <h2>Our Services</h2>
+                        <span>Sell the top quality products </span>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="service-item">
+                        <h4>Trending For Youths</h4>
+                        <p>Latest collections and accessories20% off using credit card</p>
+                        <img src="assets/images/service-01.jpg" alt="">
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="service-item">
+                        <h4>Social Media</h4>
+                        <p>you can contact our customer service on any social media platforms.</p>
+                        <img src="assets/images/service-02.jpg" alt="">
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="service-item">
+                        <h4>HOT</h4>
+                        <p>Copyright © 2022 high on trend., Ltd. All Rights Reserved.</p>
+                        <img src="assets/images/service-03.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- ***** Services Area Ends ***** -->
+
+    <!-- ***** Subscribe Area Starts ***** -->
+    <div class="subscribe">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8">
+                    <div class="section-heading">
+                        <h2>By Subscribing To Our sites You Can Get 30% Off</h2>
+                        <span>You are not allowed to suspicious works and you cannot get a Refund </span>
+                    </div>
+                    <form id="subscribe" action="" method="get">
+                        <div class="row">
+                          <div class="col-lg-5">
+                            <fieldset>
+                              <input name="name" type="text" id="name" placeholder="Your Name" required="">
+                            </fieldset>
+                          </div>
+                          <div class="col-lg-5">
+                            <fieldset>
+                              <input name="email" type="text" id="email" pattern="[^ @]*@[^ @]*" placeholder="Your Email Address" required="">
+                            </fieldset>
+                          </div>
+                          <div class="col-lg-2">
+                            <fieldset>
+                              <button type="submit" id="form-submit" class="main-dark-button"><i class="fa fa-paper-plane"></i></button>
+                            </fieldset>
+                          </div>
+                        </div>
+                    </form>
+                </div>
+                <div class="col-lg-4">
+                    <div class="row">
+                        <div class="col-6">
+                            <ul>
+                                <li>Store Location:<br><span>fully online based</span></li>
+                                <li>Phone:<br><span>6383118575</span></li>
+                                <li>Office Location:<br><span>Apsa College Sivaganga(DT) TamilNadu India Pvt</span></li>
+                            </ul>
+                        </div>
+                        <div class="col-6">
+                            <ul>
+                                <li>Work Hours:<br><span>07:30 AM - 9:30 PM Daily</span></li>
+                                <li>Email:<br><span>highontrend.com</span></li>
+                                <li>Social Media:<br><span><a href="#">Facebook</a>, <a href="#">Instagram</a>, <a href="#">Behance</a>, <a href="#">Linkedin</a></span></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    </body>
+</html>
